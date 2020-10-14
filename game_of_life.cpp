bd71e045b8d56626683a4590b8025ba1306563da
Tudor <tudoroancea@icloud.com>
2020-10-14 22:35:33 +0200

    Version GOL avecliste des vivantes optimisation de la methode evolve() et constructeurs ajout de setters

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,10 +1,6 @@
 #include "game_of_life.h"
 #include "motifs.h"
 
-bool GameOfLife::access(size_t i, size_t j) {
-    if (i>=500 || j>=500) return false;
-    else return champ[i][j];
-}
 bool GameOfLife::next_state(size_t i, size_t j) {
     int S(0);
     if (access(i - 1, j - 1)) ++S;
@@ -18,13 +14,46 @@ bool GameOfLife::next_state(size_t i, size_t j) {
     return (S==3) || (access(i, j) && S == 2);
 }
 
+// Constructeurs ========================================================================================
 GameOfLife::GameOfLife(motifs::Motif const& a_marquer, unsigned int const& C, unsigned int const& L) : nbr_gen(0), C(C), L(L) {
-   for (size_t i(0); i < 500; ++i) {
-      for (size_t j(0); j < 500; ++j) champ[i][j] = false;
+   for (size_t i(0); i < L+100; ++i) {
+      for (size_t j(0); j < C+100; ++j) champ[i][j] = false;
    }
-   for (vec::const_iterator it(a_marquer.cbegin()); it != a_marquer.cend(); ++it) champ[it->first][it->second] = true;
+   for (vec::const_iterator it(a_marquer.cbegin()); it != a_marquer.cend(); ++it) champ[(it->first)+50][(it->second)+50] = true;
+}
+GameOfLife::GameOfLife(unsigned int const& C, unsigned int const& L) : C(C), L(L), nbr_gen(0) {
+   for (auto& ligne : champ) for (auto& el : ligne) el = false;
 }
 
+// Getters & Setters ========================================================================================
+bool GameOfLife::access(size_t i, size_t j) {
+    if (i>=L+100 || j>=C+100) return false;
+    else return champ[i][j];
+}
+void GameOfLife::add_cell(std::pair<size_t,size_t> const& c) {
+   if(!access(c.first, c.second) and c.first < L+100 and c.second < C+100) {
+      vivantes.push_back(c);
+      champ[c.first][c.second] = true;
+   }
+}
+void GameOfLife::suppr_cell(std::pair<size_t,size_t> const& c) {
+   if (access(c.first, c.second)) {
+      champ[c.first][c.second] = false;
+      std::vector<std::pair<size_t,size_t>>::iterator it(vivantes.begin());
+      while (*it != c && it != vivantes.end()) {
+          ++it;
+      }
+      vivantes.erase(it);
+   }
+}
+void GameOfLife::add_motif(motifs::Motif const& m) {
+   for (std::vector<std::pair<size_t,size_t>>::const_iterator it(m.cbegin()); it != m.cend(); ++it) add_cell(*it);
+}
+void GameOfLife::suppr_motif(motifs::Motif const& m) {
+   for (std::vector<std::pair<size_t,size_t>>::const_iterator it(m.cbegin()); it != m.cend(); ++it) suppr_cell(*it);
+}
+
+// Affichage ========================================================================================
 void GameOfLife::print(std::ostream& out) const {
       out << "Debut" << std::endl;
       for (size_t i(50); i < L+50; ++i) {
@@ -37,18 +66,36 @@ void GameOfLife::print(std::ostream& out) const {
       out << " Fin | Generation num " << nbr_gen << std::endl;
 }
 
-void GameOfLife::evolve() { // todo : optimiser les zones vides
-   std::array<std::array<bool,500>,500> res;
-   for (size_t i(0); i < 500 ; ++i) {
-      for (size_t j = 0; j < 500 ; ++j) {
-         res[i][j]=next_state(i,j);
+// Evolution de la simulation  ========================================================================================
+void GameOfLife::evolve() {
+   // On crée une nouvelle liste qui contiendra les nouvelles vivantes
+   std::vector<std::pair<size_t,size_t>> nouvelles;
+
+   // On cheque chaque cellule déjà vivante pour voir si elle le restte
+   for (std::vector<std::pair<size_t,size_t>>::iterator it(vivantes.begin()); it != vivantes.end(); ++it) {
+      if (next_state(it->first, it->second)) {
+         nouvelles.push_back(*it);
       }
+      // On vérifie dans ses voisines lesquelles étaient mortes et deviendraient potentiellement vivantes
+      if (access(it->first - 1, it->second - 1)) if (next_state(it->first -1, it->second - 1)) nouvelles.push_back(*it);
+      if (access(it->first - 1, it->second)) if (next_state(it->first - 1, it->second)) nouvelles.push_back(*it);
+      if (access(it->first - 1, it->second + 1)) if (next_state(it->first - 1, it->second + 1)) nouvelles.push_back(*it);
+      if (access(it->first, it->second - 1)) if (next_state(it->first, it->second - 1)) nouvelles.push_back(*it);
+      if (access(it->first, it->second + 1)) if (next_state(it->first, it->second + 1)) nouvelles.push_back(*it);
+      if (access(it->first + 1, it->second - 1)) if (next_state(it->first + 1, it->second - 1)) nouvelles.push_back(*it);
+      if (access(it->first + 1, it->second)) if (next_state(it->first + 1, it->second)) nouvelles.push_back(*it);
+      if (access(it->first + 1, it->second + 1)) if (next_state(it->first + 1, it->second + 1)) nouvelles.push_back(*it);
    }
-   champ=res;
+   // On enlève les anciennes cellules
+   for (auto const& el : vivantes) champ[el.first][el.second] = false;
+   // On rajoute les nouvelles
+   for (auto const& el : nouvelles) champ[el.first][el.second] = true;
+   // On update la liste des vivantes et le nombre de generatitons
+   vivantes = nouvelles;
    ++nbr_gen;
 }
 
-std::array<std::array<bool,500>,500> GameOfLife::life(std::ostream& out) {
+std::vector<std::pair<size_t,size_t>> const& GameOfLife::life(std::ostream& out) {
    evolve();
-   return champ;
+   return vivantes;
 }
--- game_of_life.h
+++ game_of_life.h
@@ -7,17 +7,30 @@
 
 class GameOfLife {
 private :
-    unsigned int C,L;
-    std::array<std::array<bool,500>,500> champ;
+    unsigned int C,L;   // Partie visible de la grille
     unsigned int nbr_gen;
+    std::array<std::array<bool,500>,500> champ;     // Grille de stockage des etats des cellules
+    std::vector<std::pair<size_t,size_t>> vivantes;
 
-    bool access(size_t i, size_t j);
     bool next_state(size_t i, size_t j);
 public :
+    // Constructeurs
     GameOfLife(motifs::Motif const& a_marquer, unsigned int const& C = 50,unsigned int const& L = 50);
-    void print(std::ostream& out = std::cout) const;
+    GameOfLife(unsigned int const& C = 50, unsigned int const& L = 50);
+
+    // Getter & setters
+    bool access(size_t i, size_t j);
+    void add_cell(std::pair<size_t,size_t> const& c);
+    void suppr_cell(std::pair<size_t,size_t> const& c);
+    void add_motif(motifs::Motif const& m);
+    void suppr_motif(motifs::Motif const& m);
+
+    // Evolution de la simulaitton
     void evolve();
-    std::array<std::array<bool,500>,500> life(std::ostream& out = std::cout);
+    std::vector<std::pair<size_t,size_t>> const& life(std::ostream& out = std::cout);
+
+    // Affichage
+    void print(std::ostream& out = std::cout) const;
 };
 
 #endif // GAME_OF_LIFE_H
