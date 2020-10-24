62126a1dace0a4b8b629d01f1dba9a9e71b6d902
Tudor <tudoroancea@icloud.com>
2020-10-24 20:08:19 +0200

    Tests variables cmake

--- test/test_save_sim.cpp
+++ test/test_save_sim.cpp
@@ -5,8 +5,19 @@
 using namespace std;
 
 int main() {
-    GameOfLife G(Motif("pentominoR", "local"), 50, 50);
-    G.print();
-    G.life("pentomino1", 10);
+    //GameOfLife G(Motif("pentominoR", "local"), 50, 50);
+    //G.print();
+    //G.life("pentomino1", 10);
+    #ifdef TEST_PATH
+        cout << "test " << TEST_PATH;
+    #endif
+    cout << endl;
+    #ifdef GLOBAL_PATH
+        cout << "global " << GLOBAL_PATH;
+    #endif
+    cout << endl;
+    #ifdef SRC_PATH
+        cout << "src " << SRC_PATH;
+    #endif
     return 0;
 }
