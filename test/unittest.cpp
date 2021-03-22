2b31cd04a207e41b2b15d5b30cc5aa60d065fd42
Tudor <oancea.tudor@icloud.com>
2021-03-22 17:13:27 +0100

    Ajout des inclusions Google Test Il faut encore que je crée les premiers tests mais la lib se charge bien

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-#include "Motif.hpp"
-#include "GameOfLife.hpp"
-#include <algorithm>
-#include <vector>
-#include <iterator>
-#include <cassert>
+//
+// Created by Tudor Oancea on 21/03/2021.
+//
 
-#define assertm(exp, msg) assert(((void)msg, exp))
-
-
-int main()
-{
-    assert(2+2==4);
-    std::cout << "Execution continues past the first assert\n";
-    assertm(pow(2,2)==4, "There are five lights");
-    std::cout << "Execution continues past the second assert\n";
-    assert((2*2==4) && "Yet another way to add assert message");
-    return 0;
-}
+#include <gtest/gtest.h>
